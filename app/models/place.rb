0253c875fe3501c6cb5d60ec636ae047c7b5a63f
Cristian Salazar <cristian.salazar6@gmail.com>
2019-07-25 20:05:49 +0000

    Adding the map to our 'places page'

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,8 +2,8 @@ class Place < ApplicationRecord
   belongs_to :user
   geocoded_by :address
   after_validation :geocode
+
   validates :name, presence: true, length: { maximum: 50, minimum: 3 }
   validates :address, presence: true, length: { maximum: 140, minimum: 3 }
-  validates :description, presence: true, length: { maximum: 140, minimum: 3 }
- 
+  validates :description, presence: true, length: { maximum: 300, minimum: 3 }
 end
